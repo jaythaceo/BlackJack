c8229dd2aacf169f466f398e6f8b7f26e9b00594
jaythaceo <jaythaceo@gmail.com>
2016-12-26 13:05:14 -0500

    [chore] Added Constants

--- BlackJack.py
+++ BlackJack.py
@@ -8,3 +8,11 @@ import matplotlib.pyplot as plt
 
 from importer.StrategyImporter import StrategyImporter
 
+
+GAMES = 20000
+SHOE_SIZE = 6
+SHOE_PENETRATION = 0.25
+BET_SPREAD = 20.0
+
+DECK_SIZE = 52.0
+CARDS = {"Ace": 11, "Two": 2, "Three": 3, "Four": 4, "Five": 5, }
