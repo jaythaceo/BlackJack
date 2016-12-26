f4f6c529bf639efb359b12d9f04e8b469c036dcd
jaythaceo <jaythaceo@gmail.com>
2016-12-26 18:07:46 -0500

    [chore] Added constant fix to Cards & BET_SPREAD

--- BlackJack.py
+++ BlackJack.py
@@ -15,4 +15,13 @@ SHOE_PENETRATION = 0.25
 BET_SPREAD = 20.0
 
 DECK_SIZE = 52.0
-CARDS = {"Ace": 11, "Two": 2, "Three": 3, "Four": 4, "Five": 5, }
+CARDS = {"Ace": 11, "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10}
+BASIC_OMEGA_II = {"Ace": 0, "Two": 1, "Three": 1, "Four": 2, "Five": 2, "Six": 2, "Seven": 1, "Eight": 0, "Nine": -1, "Ten": -2, "Jack": -2, "Queen": -2, "King": -2}
+
+BLACKJACK_RULES = {
+	'triple7': False, #Count 3x7 as a blackjack
+}
+
+HARD_STRATEGY = {}
+SOFT_STRATEGY = {}
+PAIR_STRATEGY = {}
